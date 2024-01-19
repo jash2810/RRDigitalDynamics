b0f042e02a233d20da127a0d139615f9c20dd938
Jash Patel <37921859+jash2810@users.noreply.github.com>
2024-01-18 22:38:26 -0500

    Update ContactUs.jsx

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -172,7 +172,7 @@ function ContactUs() {
 }
 
 const MapsIFrame = <iframe 
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.628061387533!2d72.6497723!3d23.074093699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8132886c365d%3A0x33549bb72d9e4684!2sIndia%20Sales!5e0!3m2!1sen!2sca!4v1705358491852!5m2!1sen!2sca" 
+    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d917.6572546088015!2d72.64912676955059!3d23.07405863251152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDA0JzI2LjYiTiA3MsKwMzgnNTkuMiJF!5e0!3m2!1sen!2sca!4v1705629730899!5m2!1sen!2sca"
     width="100%" 
     height="350" 
     style={{border: 0}} 
